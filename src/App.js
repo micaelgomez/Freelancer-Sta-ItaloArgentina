37211039391f42ebd42d449c6a3d29f9c154f458
micael <mickii.gomez.mg@gmail.com>
2022-06-15 17:51:06 -0300

    bug

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import Slide from "react-reveal/Slide";
 function App() {
   return (
     <div className="app">
-      <div className=" container-wsp">
+      {/* <div className=" container-wsp">
         <a href="https://wa.me/5491158046018" rel="noreferrer" target="_blank">
           <WhatsAppIcon className="icon" />
         </a>
-      </div>
+      </div> */}
 
       <Landing />
       <Team />
